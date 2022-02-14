da6c2fc4b3da1334b6a30ea1ca58afc7f1aee466
hualala0 <1151723949@qq.com>
2022-02-14 20:48:32 +0800

    develop

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
-const fronts = document.querySelectorAll('.front')
-const backs = document.querySelectorAll('.back')
-const n = fronts.length
-const player = []
+const fronts = document.querySelectorAll(".front");
+const backs = document.querySelectorAll(".back");
+const n = fronts.length;
+const m = n / 2;
+const player = [];
 
-const keyframes = (num, n, isFront) => [
+const keyframes = (num, n, isFront, isUp) => [
   {
     transform: isFront
-      ? "rotateY(0deg)"
-      : `rotateY(-120deg) translateX(${100 / n}%)`,
+      ? "rotateY(0deg) skewY(0deg)"
+      : `rotateY(-120deg) skewY(${isUp ? "" : "-"}30deg) translateX(${
+          100 / n
+        }%)`,
     transformOrigin: `${(100 / n) * (num + 1)}%`
   },
   {
     transform: isFront
-      ? `rotateY(120deg) translateX(-${100 / n}%)`
-      : "rotateY(0deg) ",
+      ? `rotateY(120deg) skewY(${isUp ? "-" : ""}30deg) translateX(-${
+          100 / n
+        }%)`
+      : "rotateY(0deg) skewY(0deg)",
     transformOrigin: `${(100 / n) * num}%`
   }
 ];
 
 const options = {
   duration: 1000,
-  fill: 'both',
-}
+  fill: "both"
+};
 
 const playAll = () => {
-  player.forEach(value => {
-    value.play()
-  })
-}
+  player.forEach((value) => {
+    value.play();
+  });
+};
 
 for (let i = 0; i < n; i++) {
-  fronts[i].style.clipPath = `inset(0% ${Math.floor(100 - (100 / n) * (i + 1))}% 0% ${Math.floor((100 / n) * i)}%)`
-  backs[i].style.clipPath = `inset(0% ${Math.floor(100 - (100 / n) * (i + 1))}% 0% ${Math.floor((100 / n) * i)}%)`
+  const j = Math.floor(i / 2);
+  fronts[i].style.clipPath = `inset(${i % 2 == 0 ? "0%" : "50%"} ${Math.floor(
+    100 - (100 / m) * (j + 1)
+  )}% ${i % 2 != 0 ? "0%" : "50%"} ${Math.floor((100 / m) * j)}%)`;
+  backs[i].style.clipPath = `inset(${i % 2 == 0 ? "0" : "50%"} ${Math.floor(
+    100 - (100 / m) * (j + 1)
+  )}% ${i % 2 != 0 ? "0%" : "50%"} ${Math.floor((100 / m) * j)}%)`;
   player.push(
     new Animation(
-      new KeyframeEffect(fronts[i], keyframes(i, n, true), {
+      new KeyframeEffect(fronts[i], keyframes(j, m, true, i % 2 == 0), {
         ...options,
         iterations: 0.75,
-        delay: (i < n / 2 ? n / 2 - 1 - i : i - n / 2) * 200,
-      }),
-    ),
-  )
+        delay: (j < m / 2 ? m / 2 - 1 - j : j - m / 2) * 200
+      })
+    )
+  );
   player.push(
     new Animation(
-      new KeyframeEffect(backs[i], keyframes(i, n, false), {
+      new KeyframeEffect(backs[i], keyframes(j, m, false, i % 2 == 0), {
         ...options,
-        delay: (i < n / 2 ? n / 2 - 1 - i : i - n / 2) * 200,
-      }),
-    ),
-  )
+        delay: (j < m / 2 ? m / 2 - 1 - j : j - m / 2) * 200
+      })
+    )
+  );
 }
 
-playAll()
+playAll();
